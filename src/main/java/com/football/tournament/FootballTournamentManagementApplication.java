02e940445192e19e252a5af2172c046dd30efd4a
ad-munir <mounir.ayad@etu.uae.ac.ma>
2023-10-28 22:38:07 +0100

    intial

---
+++ src/main/java/com/football/tournament/FootballTournamentManagementApplication.java
@@ -0,0 +1,13 @@
+package com.football.tournament;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballTournamentManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FootballTournamentManagementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/football/tournament/FootballTournamentManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.football.tournament;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballTournamentManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
